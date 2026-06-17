58eb628b1be68ccf70e9b86ad35f0d1b3dbffaba
채다희 <cdh010126r@gmail.com>
2026-06-17 17:01:06 +0900

    refactor: (main)/layout에 공유 레이아웃 인라인, common/layout.tsx 제거

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,22 @@
-import Layout from "@/components/common/layout";
+import Sidebar from "@/components/common/Sidebar";
+import MobileBar from "@/components/common/MobileBar";
+import Footer from "@/components/common/footer";
 
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <Layout>{children}</Layout>;
+  return (
+    <div className="flex min-h-screen bg-[var(--color-bg)]">
+      <Sidebar className="hidden md:flex" />
+      <MobileBar />
+      <div className="flex-1 flex flex-col">
+        <main className="flex-1 p-8 md:p-10">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
 }
